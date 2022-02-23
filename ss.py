efae6488a0b1752f9b8278ae9f65897a7e993e87
zengxiaomu <zengxiaomu@gmail.com>
2022-02-23 11:37:50 -0500

    init
    version 0.01

--- ss.py
+++ ss.py
@@ -1,11 +1,244 @@
+import sys
 import socket
 import selectors
+import time
+import numpy as np
+
+#
+# global random sequence variables
+#
+
+seq = (0)
+seq_index = 1
+
+#
+# generate random sequnce based on probability density function 
+#
+
+def gen_random_seq(c=10):
+    #
+    # easy peasy
+    #
+    seq = np.random.exponential(1.25, 10)
+    print(seq)
+
+#
+# save random sequence to file
+#
+
+def save_seq(fn='seq.txt'):
+    print(seq)
+    # 
+    # save to file for re-use 
+    # This could be useful if generating
+    # random exponential sequence in C code
+    # is too hard...just read it from a file!
+    #
+
+#
+# read random sequence from file
+#
+
+def read_seq(fn='seq.txt'):
+    print(seq)
+    #
+    # in case numpy is not available..read it from a file
+    #
+    
+#
+# VoIP 
+#
+
+def send_active_packet(size=122):
+    print(f"send active packet size {size}")
+    #
+    # How to actually send VoIP packets in python? Maybe ask Tomas?
+    #
+
+def send_silent_packet(size=0):
+    print(f"send silent packet size {size}")
+    #
+    # How to actually send VoIP packets in python? 
+    #
+
+def talking_state():
+    duration = seq[seq_index]
+    seq_index += 1
+    #
+    # during the duration, generate 12.2K data per seconds
+    #
+    block = int(duration*100)
+    #
+    # One block is 10 ms, we shall send 122 bytes per block
+    #
+    for i in range(0, block, 1):
+        send_active_packet(122)
+        
+def silent_state():
+    duration = seq[seq_index]
+    seq_index += 1
+    #
+    # during the duration, generate ??? data per seconds
+    # we need to send "silent packet" but what does that
+    # actually mean? 
+    #
+    block = int(duration*100)
+    #
+    # One block is 10 ms, we shall send 0? bytes per block
+    #
+    for i in range(0, block, 1):
+        send_silent_packet(0)
+        
+
+#
+# VoIP Call manager
+#
+
+def call_manager(total=10, duration=10):
+    print(f"total calls: {total} total time: {duration}")
+    #
+    # Simulate total=10 number of calls
+    #
+    # SO, should I use threads with 1 thread per call? 
+    #
+    # No. Only use threads as last resort. 
+    #
+    # If efficency is a concern, we can implement threads in C code later
+    #
+    block = int(duration*100)
+    #
+    # init call state
+    for i in range(0, total-1, 1):
+        call_state[i] = int(np.random(1)) # 0 or 1
+        timer[i] = seq[seq_index]
+        seq_index += 1
+    #
+    # Now start call processes
+    #
+    for b in range(0, block, 1):
+        for i in range(0, total-1, 1):
+            if call_state[i] == 0:
+                send_silent_packet(0)
+            elif call_state[i] == 1:
+                send_active_packet(122)
+            if b == timer[i]:
+                #
+                # transition between talk state and silent state
+                #
+                if call_state[i] == 0:
+                    call_state[i] = 1
+                    timer[i] += seq[seq_index]
+                    seq_index+= 1
+                elif call_state[i] == 1:
+                    call_state[i] = 0
+                    timer[i] += seq[seq_index]
+                    seq_index += 1
+        time.sleep(0.01)  # sleep for 10 ms
+
+
+#
+# Networking default values
+#
+
+default_host='127.0.0.1'
+default_port=1729
+
+#
+# global variables sel
+#
 
 sel = selectors.DefaultSelector()
 
+#
+# start socket server
+#
+
+def init_server(host=default_host, port=default_port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind((host, port))
+        s.listen()
+    except OSError as msg:
+        print(msg)
+        return 0
+    print(f"listening on {(host, port)}")
+    s.setblocking(False)
+    sel.register(s, selectors.EVENT_READ, data=None)
+    return s
+
+#
+# global variables ss
+#
+
+ss = init_server()
+
+#
+# start socket client
+#
+
+def init_conn(host=default_host, port=default_port):
+    print(f"Starting connection to {(host, port)}")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    err = s.connect_ex((host, port))
+    if err != 0:
+        print(f"Connection failed: {err}")
+        return err
+    else:
+        s.setblocking(False)
+        sel.register(s, selectors.EVENT_READ | selectors.EVENT_WRITE, data=None)
+        return s
+
+#
+# accept client connection
+#
+
+def accept_conn(s):
+    s.setblocking(True)
+    conn, addr = s.accept()
+    s.setblocking(False)
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    sel.register(conn, selectors.EVENT_READ | selectors.EVENT_WRITE, data=None)
+
+#
+# receive client message
+#
+
+def service_conn(key, mask):
+    s = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        print(f"receive data from {data.addr}")
+        recv_data = sock.recv(1024)
+        print(f"done")
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(s)
+            s.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Sending {data.outb!r} to {data.addr}")
+            sent = s.send(data.outb)
+            data.outb = data.outb[sent:]
+
+#
+# main loop for handling socket events
+#
+
 def server_loop():
-    while True:
-        events = sel.select(timeout=None)
-        for key, mask in events:
-            print("key.data: %s\n" % key.data)
+    try:
+        while True:
+            events = sel.select(timeout=1)
+            for key, mask in events:
+                print("key.data: %s" % key.data)
+                if key.data == None:
+                    accept_conn(ss)
+                else:
+                    service_conn(key, mask)
+    except KeyboardInterrupt:
+        print("keyboard interrupt, exiting")
+    finally:
+        sel.close()
 
